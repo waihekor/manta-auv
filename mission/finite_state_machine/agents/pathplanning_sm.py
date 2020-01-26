45b84b8310f5d5a48715e38382ce6d7450679688
olaals <olaalstad96@gmail.com>
2020-01-26 18:03:09 +0100

    modifisert state machine

--- mission/finite_state_machine/agents/pathplanning_sm.py
+++ mission/finite_state_machine/agents/pathplanning_sm.py
@@ -14,6 +14,29 @@ from geometry_msgs.msg import Pose, Point, Quaternion
 
 from tf.transformations import quaternion_from_euler
 
+
+class Foo(State):
+    
+    def __init__(self):
+        State.__init__(self, outcomes = ["outcome1", "outcome3"])
+    
+    def execute(self, userdata):
+        print("Starting state Foo")
+        rospy.sleep(3)
+        print("Waited 3 seconds")
+        return "outcome3"
+
+class Bar(State):
+    
+    def __init__(self):
+        State.__init__(self, outcomes = ["outcome2"])
+    
+    def execute(self, userdata):
+        print("Starting state Bar")
+        rospy.sleep(2)
+        print("Waited 2 seconds")
+        return "outcome2"
+
 def make_los_goal(x_prev, y_prev, x_next, y_next, depth, speed=0.20, sphere_of_acceptance=0.5):
     
     los_goal = LosPathFollowingGoal()
@@ -38,9 +61,8 @@ class TaskManager():
 
         rospy.init_node('pathplanning_sm', anonymous=False)
 
-        #rospy.on_shutdown(self.shutdown)
 
-        patrol = StateMachine([])
+        patrol = StateMachine(outcomes = ["outcome_patrol"])
 
         print("sm init")
 
@@ -49,7 +71,7 @@ class TaskManager():
             StateMachine.add(   'CP1',
                                 SimpleActionState(  'los_path',
                                                     LosPathFollowingAction,
-                                                    make_los_goal(0.0, 0.0, 4.0, 4.0, -0.5)),
+                                                    make_los_goal(0.0, 0.0, 9.0, 2.0, -0.5)),
                                 transitions = { 'succeeded': 'CP2',
                                                 'aborted': 'CP1',
                                                 'preempted': 'CP2'})
@@ -62,10 +84,22 @@ class TaskManager():
                                                 'aborted': 'CP2',
                                                 'preempted': 'CP1'})
 
+        #rospy.on_shutdown(self.shutdown)
+
+        hsm = StateMachine([])
+        with hsm:
+            StateMachine.add('FOO', Foo(), transitions={"outcome1": 'BAR', "outcome3": 'BAS'})
+            StateMachine.add('BAR', Bar(), transitions={"outcome2": 'FOO'})
+            StateMachine.add('BAS', patrol, transitions={"outcome_patrol": 'BAR'})
+            
+        hsm.execute()
+
+        
+
 
         intro_server = IntrospectionServer(str(rospy.get_name()), patrol,'/SM_ROOT')
         intro_server.start()
-        patrol.execute()
+        #patrol.execute()
         print("State machine execute finished")
         intro_server.stop()
         rospy.spin()
@@ -73,6 +107,13 @@ class TaskManager():
     def shutdown(self):
         rospy.loginfo("stopping the AUV...")
         rospy.sleep(10)
+
+
+
+    
+
+    
+        
 
 
 if __name__ == '__main__':
